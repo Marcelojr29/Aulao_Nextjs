638631bbd54782f857f3d9ff03c1b05515b97340
Marcelo Junior <Marcelo.Junior@fit-tecnologia.org.br>
2025-02-25 08:27:04 -0400

    Estilizações e componente Navegador

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,13 +1,13 @@
-import styles from '@/styles/Estiloso.module.css'
-import Link from 'next/link'
-import Layout from '@/components/Layout'
+import styles from "@/styles/Estiloso.module.css";
+import Link from "next/link";
+import Layout from "@/components/Layout";
 
 export default function Estiloso() {
-    return (
-        <Layout titulo="Exemplo de CSS Modularizado">
-            <div className={styles.roxo}>
-                <h1>Estilo usando CSS Módulos</h1>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout titulo="Exemplo de CSS Modularizado">
+      <div className={styles.roxo}>
+        <h1>Estilo usando CSS Módulos</h1>
+      </div>
+    </Layout>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,19 @@
-import Link from "next/link"
+import Navegador from "@/components/Navegador";
+import Link from "next/link";
 export default function Inicio() {
-    return (
-        <div>
-            <Link href="/estiloso">
-                Estiloso
-            </Link>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        height: "100vh",
+      }}
+    >
+      <Navegador texto="Estiloso" destino="/estiloso" />
+      <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3" />
+      <Navegador texto="JSX" destino="/jsx" cor="crimson" />
+    </div>
+  );
 }
